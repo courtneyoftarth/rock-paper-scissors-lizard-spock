d0cf71ba6284cdfd1b2b235fd65eee19a485f41a
Courtney <dowelc@cs.washington.edu>
2020-01-18 15:59:10 -0800

    Working handler for choices endpoint

--- controller.py
+++ controller.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
+from data.choices import getChoices
+
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/choices")
+def choices():
+    return getChoices()
 
 if __name__ == "__main__":
     app.run()
---
+++ data/choices.py
@@ -0,0 +1,5 @@
+
+
+
+def getChoices():
+    return '''[{id: 1, name: "rock"}, {id: 2, name: "paper"}, {id: 3, name: "scissors"}, {id: 4, name: "lizard"}, {id: 5, name: "spock"}]'''
