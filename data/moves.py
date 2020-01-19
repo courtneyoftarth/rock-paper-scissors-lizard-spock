29647bdfc8069790d2d8a03c4bf6937bd074625b
Courtney <dowelc@cs.washington.edu>
2020-01-19 10:26:39 -0800

    Put moves in their own file

---
+++ data/moves.py
@@ -0,0 +1,27 @@
+moves = [
+    {
+        "beats": [3, 4],
+        "id": 1,
+        "name": "rock"
+    },
+    {
+        "beats": [1, 5],
+        "id": 2,
+        "name": "paper"
+    },
+    {
+        "beats": [2, 4],
+        "id": 3,
+        "name": "scissors"
+    },
+    {
+        "beats": [2, 5],
+        "id": 4,
+        "name": "lizard"
+    },
+    {
+        "beats": [1, 3],
+        "id": 5,
+        "name": "spock"
+    }
+]
